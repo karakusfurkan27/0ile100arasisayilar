8ae6ae84cd135180fb88dd0ec85baa07fd998fd9
Furkan Karakuş <m.furkan.karakus27@gmail.com>
2024-04-27 10:33:28 +0300

    0ile100arasısayılar
    0ile100arasısayılar

---
+++ 0ile100arasısayılar/0ile100arasısayılar/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int sayi;
+        Console.Write("Sayıyı Girin : ");
+        sayi = Convert.ToInt32(Console.ReadLine());
+        if (sayi >= 0 && sayi <= 100)
+        {
+            Console.WriteLine("Girilen sayı 0-100 aralığında");
+        }
+        else
+        {
+            Console.WriteLine("Girilen sayı 0-100 aralığında değil");
+        }
+        Console.ReadKey();
+    }
+
+    
+    
+      
+    }
